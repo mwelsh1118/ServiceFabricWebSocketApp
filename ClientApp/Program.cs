939e9163429cd781720f158f5fdc14ffe288066c
Mark Welsh <mwelsh1118@gmail.com>
2018-06-27 20:44:22 -0500

    Initial commit

---
+++ ClientApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        public static async Task RunAsync()
+        {
+            using (var webSocket = new ClientWebSocket())
+            {
+                await webSocket.ConnectAsync(new Uri("ws://localhost:8280/ws"), CancellationToken.None);
+                var bytes = Encoding.UTF8.GetBytes("Hello from the client!");
+                await webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                bytes = new byte[1024];
+                var result = await webSocket.ReceiveAsync(bytes, CancellationToken.None);
+                Console.WriteLine(Encoding.UTF8.GetString(new ArraySegment<byte>(bytes, 0, result.Count).ToArray()));
+            }
+        }
+    }
+}
---
+++ WebService/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Fabric;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebService
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, StatelessServiceContext serviceContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            
+            app.UseWebSockets();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(serviceContext, context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private async Task Echo(StatelessServiceContext serviceContext, HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                var bytes = new ArraySegment<byte>(buffer, 0, result.Count).ToArray();
+                var @string = Encoding.UTF8.GetString(bytes);
+
+                ServiceEventSource.Current.ServiceMessage(serviceContext, @string);
+
+                @string = @string.Replace("client", "server");
+                await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(@string)), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
